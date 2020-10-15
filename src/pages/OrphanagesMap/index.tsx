683090d7eb0d64f0846ff9c95038777b10fa349b
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-15 05:28:14 -0300

    :sparkles: feat: add map marker

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import { LogoMarker } from '../../images';
 
 import { Container, SideBar, ButtonAdd } from './styles';
+import mapIcon from '../../utils/mapIcon';
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -32,6 +34,20 @@ const OrphanagesMap: React.FC = () => {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+
+        <Marker icon={mapIcon} position={[-27.2092052, -49.6401092]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="/">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <ButtonAdd>
