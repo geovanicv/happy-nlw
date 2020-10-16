aca562df18df48c1b84b25db9cba0f2b756a3224
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-15 23:02:12 -0300

    :hammer: chore: update Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,27 +5,13 @@ import { Container } from './styles';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
-  haveInput?: boolean;
-  textarea?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({
-  textarea = false,
-  haveInput = true,
-  label,
-  name,
-  children,
-  ...rest
-}) => {
+const Input: React.FC<InputProps> = ({ label, name, children, ...rest }) => {
   return (
     <Container>
       <label htmlFor={name}>{label}</label>
-
-      {textarea ? (
-        <textarea id={name} maxLength={300} />
-      ) : (
-        haveInput && <input type="text" id={name} {...rest} />
-      )}
+      <input type="text" id={name} {...rest} />
 
       {children}
     </Container>
