5da03264e87c9511de3deae4687f559539899fdc
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-21 23:33:51 -0300

    :art: chore: add alert pages to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,8 @@ import OrphanagesMap from '../pages/OrphanagesMap';
 import Orphanage from '../pages/Orphanage';
 import CreateOrphanage from '../pages/CreateOrphanage';
 import SignIn from '../pages/Login/SignIn';
+import SuccessMessage from '../components/AlertMessage/SuccessMessage';
+import ConfirmMessage from '../components/AlertMessage/ConfirmMessage';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
@@ -17,6 +19,9 @@ const Routes: React.FC = () => (
       <Route path="/orphanages/:id" component={Orphanage} />
 
       <Route path="/login" component={SignIn} />
+
+      <Route path="/alert/success" component={SuccessMessage} />
+      <Route path="/alert/confirm" component={ConfirmMessage} />
     </Switch>
   </BrowserRouter>
 );
