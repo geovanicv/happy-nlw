3547f30a7921eabb56b8f92223be6ce451fc90e2
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-22 04:03:29 -0300

    :sparkles: feat: create dashboard side bar component

---
+++ src/pages/Dashboard/RegistredOrphanages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import Header from '../Header';
+
+const RegistredOrphanages: React.FC = () => {
+  return <Header />;
+};
+
+export default RegistredOrphanages;
