067c435cf293b9f356719fcae919cc8a3cefa675
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-21 23:36:45 -0300

    :bento: chore: add images for alert pages

--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,3 +1,5 @@
 export { ReactComponent as Logo } from './logo.svg';
 export { ReactComponent as LogoMarker } from './logo-marker.svg';
 export { ReactComponent as Logotipo } from './logotipo.svg';
+export { ReactComponent as SuccessAlert } from './successAlert.svg';
+export { ReactComponent as ConfirmAlert } from './confirmAlert.svg';
