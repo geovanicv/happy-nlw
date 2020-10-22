23904a5575a25aac0f755fa30413c8047ac8b861
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-10-21 23:33:06 -0300

    :art: chore: add eye and eyeoff to input type password

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,7 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-import { Container } from './styles';
+import { Container, InputContainer } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -14,10 +15,24 @@ const Input: React.FC<InputProps> = ({
   children,
   ...rest
 }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const Icon = isVisible ? <FiEye size={26} /> : <FiEyeOff size={26} />;
+  const passwordType = isVisible ? 'text' : 'password';
+
   return (
     <Container>
       <label htmlFor={name}>{label}</label>
-      <input type={type || 'text'} id={name} {...rest} />
+      <InputContainer>
+        <input
+          type={type === 'password' ? passwordType : type}
+          id={name}
+          {...rest}
+        />
+        <button type="button" onClick={() => setIsVisible(prev => !prev)}>
+          {type === 'password' && Icon}
+        </button>
+      </InputContainer>
 
       {children}
     </Container>
